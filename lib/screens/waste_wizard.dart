7372e053b691bb91226c8b6b4714fba230920af9
SWEEPC2 <carterdbroom@gmail.com>
2024-02-20 18:01:52 -0500

    Created pages on WasteWizard for guessing. May need more UI updates later.

--- lib/screens/waste_wizard.dart
+++ lib/screens/waste_wizard.dart
@@ -1,7 +1,9 @@
 import 'package:binbuddy_frontend/net/disposal.dart';
 import 'package:binbuddy_frontend/net/vision.dart';
+import 'package:binbuddy_frontend/screens/home.dart';
 import 'package:binbuddy_frontend/screens/widgets/bottom_nav_bar.dart';
 import 'package:binbuddy_frontend/screens/widgets/camera.dart';
+import 'package:binbuddy_frontend/screens/widgets/login_button.dart';
 import 'package:binbuddy_frontend/screens/widgets/query_map.dart';
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
@@ -56,8 +58,8 @@ class _WasteWizardPageState extends State<WasteWizardPage> {
         
         var backButton = IconButton(
           icon: const Icon(Icons.arrow_back_outlined),
-          onPressed: () => {
-            setMode("")
+          onPressed: () {
+            setMode("");
           },
         );
 
@@ -84,19 +86,125 @@ class _WasteWizardPageState extends State<WasteWizardPage> {
 
 
 
-        if(mode == "") {
+        if (mode == "") {
           return SelectMode(setMode: setMode, bar: bar);
         }
 
-
-
-        if(camera != null && foundDisposal == null) {
+        if (camera != null && foundDisposal == null) {
           return Scaffold(
             appBar: barWithBack,
             body: TakePictureScreen(camera: camera!, callAfter: afterImage),
             bottomNavigationBar: const Bottom(),
           );
-        } else if(camera != null && foundDisposal != null) {
+        } else if (camera != null && foundDisposal != null && mode == "Track Waste") {
+          return Scaffold(
+            appBar: bar,
+            body: Column(
+              children: [
+                const Text(
+                  "Scan Complete!",
+                  style: TextStyle(
+                    fontFamily: "Monospace",
+                  )
+                ),
+                const Text(
+                  "Where Should It Go?",
+                  style: TextStyle(
+                    fontFamily: "Monospace",
+                  ),
+                ),
+                Row(
+                  children: [
+                    LoginButton(
+                      onTap:() {
+                        if (foundDisposal!.locationKey == "Garbage"){
+                          setMode("Correct");
+                        } else {
+                          setMode("Incorrect");
+                        }
+                      },
+                      child: const Text(
+                        "Garbage",
+                      ),
+                    ),
+                    LoginButton(
+                      onTap:() {
+                        if (foundDisposal!.locationKey == "Compost"){
+                          setMode("Correct");
+                        } else {
+                          setMode("Incorrect");
+                        }
+                      },
+                      child: const Text(
+                        "Compost",
+                      ),
+                    ),
+                    LoginButton(
+                      onTap:() {
+                        if (foundDisposal!.locationKey == "Recycling"){
+                          setMode("Correct");
+                          
+                        } else {
+                          setMode("Incorrect");
+                        }
+                      },
+                      child: const Text(
+                        "Recycling",
+                      ),
+                    ),           
+                  ],
+                ),
+              ],
+            ),
+            bottomNavigationBar: const Bottom(),
+          );
+        } else if (camera != null && foundDisposal != null && mode == "Correct") {
+          return Scaffold(
+            backgroundColor: Colors.green,
+            body: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Icon(Icons.check_rounded),
+                LoginButton(
+                  onTap: () {
+                    Navigator.pushReplacement(context, 
+                    MaterialPageRoute(builder: (context) => HomePage(garbageValue: 0, compostValue: 0, recyclingValue: 0,)),
+                    );
+                  },
+                  child: const Text(
+                    "Continue",
+                    style: TextStyle(
+                      fontFamily: "Monospace",
+                    ),
+                  ),
+                ),
+              ],           
+            ),
+          );
+        } else if (camera != null && foundDisposal != null && mode == "Correct") {
+          return Scaffold(
+            backgroundColor: Colors.green,
+            body: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Icon(Icons.close_rounded),
+                LoginButton(
+                  onTap: () {
+                    Navigator.pushReplacement(context, 
+                    MaterialPageRoute(builder: (context) => HomePage(garbageValue: 0, compostValue: 0, recyclingValue: 0,)),
+                    );
+                  },
+                  child: const Text(
+                    "Continue",
+                    style: TextStyle(
+                      fontFamily: "Monospace",
+                    ),
+                  ),
+                ),
+              ],           
+            ),
+          );
+        } else if (camera != null && foundDisposal != null && mode == "Discover") {
           return Scaffold (
             appBar: bar,
             body: QueryMap(
