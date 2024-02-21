8ce92b2818406967056a54d72f059afb569c91d9
AustinBray77 <austin.bray77@gmail.com>
2024-02-21 11:06:51 -0500

    Updates to waste wizard

--- lib/net/disposal.dart
+++ lib/net/disposal.dart
@@ -14,10 +14,11 @@ enum DisposalLocation {
 class Disposal {
   DisposalLocation location;
   String locationKey;
+  String description;
   List<Marker>? searchedLocations;
   Position? disposalPosition;
 
-  Disposal(this.location, this.locationKey);
+  Disposal(this.location, this.locationKey, this.description);
 
   static Future<Disposal> getDisposalLocation(XFile imgFile) async {
       final props = await Vision.getImageProperties(imgFile, 0.5);
@@ -31,9 +32,29 @@ class Disposal {
       return disposal;
   }
 
-  static var garbage = Disposal(DisposalLocation.garbage, "Garbage");
-  static var recycling = Disposal(DisposalLocation.recycling, "Recycling");
-  static var compost = Disposal(DisposalLocation.compost, "Compost");
-  static var electronic = Disposal(DisposalLocation.unknown, "Electronic Recycling Depot");
-  static var textile = Disposal(DisposalLocation.unknown, "Textile Recycling Depot");
+  static var garbage = Disposal(
+    DisposalLocation.garbage, 
+    "Garbage",
+    "Look for a black bin usually signed with Waste or Garbage!"
+    );
+  static var recycling = Disposal(
+    DisposalLocation.recycling, 
+    "Recycling",
+    "Look for a blue bin usually signed with Recycling or a three arrow symbol.\nBe careful as some recycling bins are specific to certain items only, like paper or glass"
+    );
+  static var compost = Disposal(
+    DisposalLocation.compost, 
+    "Compost",
+    "Look for a green bin usually signed with Compost!"
+    );
+  static var electronic = Disposal(
+    DisposalLocation.unknown, 
+    "Electronic Recycling Depot",
+    "These locations take in any electronic parts and help to recycle and dispose of them correctly!"
+    );
+  static var textile = Disposal(
+    DisposalLocation.unknown, 
+    "Textile Recycling Depot",
+    "These location take any textile based items (clothes, sheets, fabrics) and help to recycle / redistribute them!"
+    );
 }
